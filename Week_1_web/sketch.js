102cb32070b6cc7f7889b7d67b3a690a5ea20828
faiv <rise.05.am@gmail.com>
2022-12-06 18:56:37 +0500

    my code practise

---
+++ Week_1_web/sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(31,51,90);
+  noStroke();
+  fill(160,0,0);
+  ellipse(200,200,120,120);
+  square(200,200,200,250,5);
+}
